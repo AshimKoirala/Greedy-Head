55bb6a40f5cc0a4e3ca6bf40019f8cfa7e77acae
Ashim Koirala <ashimkoirala01@gmail.com>
2024-03-22 11:10:05 +0545

    updating

--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
-/* NOTE: things needed to be added and manage
-  -total coin count and highscore needed to be updated
-  -shop feature providing different heads
-  -else part of function update() needs to be change for velocity to be stoped
-    (the written code works but makes home button unable to be clicked)
-*/
 var config = {
   type: Phaser.AUTO,
   width: 1000,
@@ -25,11 +19,13 @@ var config = {
 var game = new Phaser.Game(config);
 var player;
 var coin;
+var obstacle;
 var gameStarted = false;
 var timer;
 var gameDuration = 15000;
 var endTime;
 var timerText;
+var scoreText; // Added to avoid scoreText undefined error
 
 function preload() {
   this.load.image("player", "assets/images/default.png");
@@ -37,6 +33,7 @@ function preload() {
   this.load.image("startButton", "assets/images/start.png");
   this.load.image("timerchallengeButton", "assets/images/timerchallenge.png");
   this.load.image("shopButton", "assets/images/shop.png");
+  this.load.image("obstacle", "assets/images/spike.png");
 }
 
 function create() {
@@ -65,169 +62,148 @@ function create() {
   highscore.setText("High Score: " + highscore);
 
   startButton.on("pointerdown", () => {
-    startButton.destroy(); // Remove the images/texts displayed
+    startButton.destroy();
     timerchallengeButton.destroy();
     shopButton.destroy();
     playerimg.destroy();
     highscore.destroy();
     Totalcoin.destroy();
     gameStarted = true;
-    startgame();
+    startgame.call(this); // Ensure correct context
+    createObstacle.call(this); // Call createObstacle after the buttons are destroyed
   });
-  // when timer challenged button is clicked
+
   timerchallengeButton.on("pointerdown", () => {
-    startButton.destroy(); // Remove the images/texts displayed
+    startButton.destroy();
     timerchallengeButton.destroy();
     shopButton.destroy();
     playerimg.destroy();
     highscore.destroy();
     Totalcoin.destroy();
     gameStarted = true;
-    startTimerChallenge();
+    startTimerChallenge.call(this); // Ensure correct context
+    createObstacle.call(this); // Call createObstacle after the buttons are destroyed
   });
 
-  // when shop button is clicked
   shopButton.on("pointerdown", () => {
-    startButton.destroy(); // Remove the images/texts displayed
+    startButton.destroy();
     timerchallengeButton.destroy();
     shopButton.destroy();
     playerimg.destroy();
     highscore.destroy();
     Totalcoin.destroy();
     gameStarted = true;
-    shop();
+    shop.call(this); // Ensure correct context
+    createObstacle.call(this); // Call createObstacle after the buttons are destroyed
   });
+}
 
-  //-----------------Below is the Get started-------------------------------------------
+function startgame() {
+  player = this.physics.add.sprite(0, 650, "player");
+  player.setScale(0.2);
+  player.setCollideWorldBounds(true);
+  respawnCoin();
+  Totalcoin = 0;
+  scoreText = this.add.text(16, 16, "Coin: " + Totalcoin, {
+    fontSize: "32px",
+    fill: "#FFFFFF",
+  });
+  this.physics.add.collider(player, coin, collectCoin, null, this);
+}
 
-  const startgame = () => {
-    // player sprite
-    player = this.physics.add.sprite(0, 650, "player");
-    player.setScale(0.2);
-    // player physics properties
-    player.setCollideWorldBounds(true);
+function startTimerChallenge() {
+  player = this.physics.add.sprite(0, 650, "player");
+  player.setScale(0.2);
+  player.setCollideWorldBounds(true);
+  respawnCoin();
+  Totalcoin = 0;
+  scoreText = this.add.text(16, 16, "Coin: " + Totalcoin, {
+    fontSize: "32px",
+    fill: "#FFFFFF",
+  });
+  endTime = this.time.now + gameDuration;
+  timer = this.time.addEvent({
+    delay: gameDuration,
+    callback: endGame,
+    callbackScope: this,
+  });
+  timerText = this.add.text(850, 16, "", {
+    fontSize: "32px",
+    fill: "#FFFFFF",
+  });
+  this.time.addEvent({
+    delay: 1000,
+    callback: updateTimer,
+    callbackScope: this,
+    loop: true,
+  });
+  this.physics.add.collider(player, coin, collectCoin, null, this);
+}
 
-    // Create initial coin
-    respawnCoin();
+function respawnCoin() {
+  if (coin) {
+    coin.destroy();
+  }
+  coin = this.physics.add.sprite(
+    Phaser.Math.Between(0, 1000),
+    Phaser.Math.Between(0, 700),
+    "coin"
+  );
+  coin.setScale(0.1);
+}
 
-    // coin count
-    Totalcoin = 0;
+function collectCoin() {
+  coin.disableBody(true, true);
+  Totalcoin++;
+  scoreText.setText("Coin: " + Totalcoin);
+  respawnCoin();
+  this.physics.add.collider(player, coin, collectCoin, null, this);
+}
 
-    // score text
-    scoreText = this.add.text(16, 16, "Coin: " + Totalcoin, {
-      fontSize: "32px",
-      fill: "#FFFFFF",
-    });
-    // If player touches coin, score increases and coin reappear in another location
-    this.physics.add.collider(player, coin, collectCoin, null, this);
-  };
-
-  //----------------Below is the TIMER challenge----------------------------------
-  const startTimerChallenge = () => {
-    // player sprite
-    player = this.physics.add.sprite(0, 650, "player");
-    player.setScale(0.2);
-    // player physics properties
-    player.setCollideWorldBounds(true);
-
-    // Create initial coin
-    respawnCoin();
-
-    // coin count
-    Totalcoin = 0;
-
-    // score text
-    scoreText = this.add.text(16, 16, "Coin: " + Totalcoin, {
-      fontSize: "32px",
-      fill: "#FFFFFF",
-    });
-
-    // Set the end time for the game
-    endTime = this.time.now + gameDuration;
-
-    // Start the game timer
-    timer = this.time.addEvent({
-      delay: gameDuration,
-      callback: endGame,
-      callbackScope: this,
-    });
-
-    // Display the timer text
-    timerText = this.add.text(850, 16, "", {
-      fontSize: "32px",
-      fill: "#FFFFFF",
-    });
-
-    // Update the timer display every second
-    this.time.addEvent({
-      delay: 1000,
-      callback: updateTimer,
-      callbackScope: this,
-      loop: true,
-    });
-
-    // If player touches coin, score increases and coin reappear in another location
-    this.physics.add.collider(player, coin, collectCoin, null, this);
-  };
-
-  //respawn the coin
-  const respawnCoin = () => {
-    if (coin) {
-      coin.destroy(); // Destroy previous coin
-    }
-    coin = this.physics.add.sprite(
+function createObstacle() {
+  if (!obstacle) {
+    obstacle = this.physics.add.sprite(
       Phaser.Math.Between(0, 1000),
       Phaser.Math.Between(0, 700),
-      "coin"
+      "obstacle"
     );
-    coin.setScale(0.1);
-  };
+    obstacle.setScale(0.1);
+    this.physics.add.overlap(player, obstacle, endGame, null, this);
+  }
+}
 
-  //coin collection
-  const collectCoin = () => {
-    coin.disableBody(true, true);
-    Totalcoin++;
-    scoreText.setText("Coin: " + Totalcoin);
-    respawnCoin(); // Respawn the coin
+function endGame() {
+  console.log("Game Over");
+  gameStarted = false;
+  var gameOverText = this.add.text(350, 250, "Game Over", {
+    fontSize: "64px",
+    fill: "#FFFFFF",
+  });
+  var HomeText = this.add.text(450, 350, "Home", {
+    fontSize: "32px",
+    fill: "#FFFFFF",
+    backgroundColor: "#808080",
+    padding: {
+      x: 10,
+      y: 5,
+    },
+    fixedWidth: 100,
+    fixedHeight: 40,
+    align: "center",
+  });
+  HomeText.setInteractive();
+  HomeText.on("pointerdown", () => {
+    this.scene.restart();
+  });
+}
 
-    this.physics.add.collider(player, coin, collectCoin, null, this);
-  };
-
-  const endGame = () => {
-    console.log("Game Over");
-    gameStarted = false;
-    var gameOverText = this.add.text(350, 250, "Game Over", {
-      fontSize: "64px",
-      fill: "#FFFFFF",
-    });
-
-    var HomeText = this.add.text(450, 350, "Home", {
-      fontSize: "32px",
-      fill: "#FFFFFF",
-      backgroundColor: "#808080",
-      padding: {
-        x: 10,
-        y: 5,
-      },
-      fixedWidth: 100,
-      fixedHeight: 40,
-      align: "center",
-    });
-    HomeText.setInteractive();
-    HomeText.on("pointerdown", () => {
-      this.scene.restart(); // Restart the game
-    });
-  };
-  //update the timer display
-  const updateTimer = () => {
-    var remainingTime = Math.max(0, endTime - this.time.now);
-    var seconds = Math.ceil(remainingTime / 1000);
-    timerText.setText("Time: " + seconds);
-  };
+function updateTimer() {
+  var remainingTime = Math.max(0, endTime - this.time.now);
+  var seconds = Math.ceil(remainingTime / 1000);
+  timerText.setText("Time: " + seconds);
 }
 
 function update() {
-  // Player movement
   if (gameStarted) {
     var cursors = this.input.keyboard.createCursorKeys();
     if (cursors.left.isDown) {
@@ -242,11 +218,5 @@ function update() {
       player.setVelocityX(0);
       player.setVelocityY(0);
     }
-  } else {
-    // If soon as timer hits 0 player stuck at that exact position
-    // if (this.time.now > endTime) {
-    //    player.setVelocityX(0);
-    //    player.setVelocityY(0);
-    //  }
   }
 }
